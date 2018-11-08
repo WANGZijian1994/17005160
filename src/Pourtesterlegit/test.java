0bb5e853dcff9dd0a7af89d518ef92a50ef72202
WANGZijian1994 <wang zijian@wangzj>
2018-11-09 00:01:42 +0100

    Plaisir

---
+++ src/Pourtesterlegit/test.java
@@ -0,0 +1,13 @@
+package Pourtesterlegit;
+
+public class test {
+
+	public static void main(String[] args) {
+		char ch='s';
+		char sh='t';
+		System.out.print(ch+sh+"是s和t对应ASCII码值的和");
+		System.out.print("\n"+"s and t: "+ch+sh);
+
+	}
+
+}
