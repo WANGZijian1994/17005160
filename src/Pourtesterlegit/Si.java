e9c92e9b7aae6628b5d4ff6b7767119e4de45d14
wang zijian <wang zijian@10.8.25.115>
2018-11-15 17:00:45 +0100

    Si

--- src/Pourtesterlegit/Si.java
+++ src/Pourtesterlegit/Si.java
@@ -3,7 +3,7 @@ package Pourtesterlegit;
 public class Si {
 
 	public static void main(String[] args) {
-		int a=2,b=7;
+		int a=3,b=7;
 		int absolute;
 		absolute=a>b?a-b:b-a;
 		System.out.println(absolute);
